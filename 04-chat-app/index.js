26d86a6125fdc87edd3095ad8edd1d5adc0b0602
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-25 03:22:57 +0630

    Single file upload

--- 04-chat-app/index.js
+++ 04-chat-app/index.js
@@ -1,69 +1,26 @@
-import express from 'express'
+import express from "express";
 import * as http from "http";
 import 'dotenv/config'
-import bodyParser from "body-parser";
-import jwt from 'jsonwebtoken'
-import {ExtractJwt, Strategy as JwtStrategy} from 'passport-jwt'
-import passport from "passport";
+import multer from "multer";
 
-const opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.SECRET_KEY;
-
-
-const jwtStrategy = new JwtStrategy(opts, function (payload, done) {
-	const validated = payload.email === 'hello@test.com'
-	if (validated) {
-		done(null, {data: true})
-	} else {
-		done('Unauntenicated')
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './images')
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+		cb(null, uniqueSuffix + "-" + file.originalname)
 	}
 })
-passport.use(jwtStrategy)
-
-
-const token = jwt.sign({
-	email: 'hello@test.com',
-}, process.env.SECRET_KEY)
+const upload = multer({storage})
 
 const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const db = new Map()
-
-app.get('/', (req, res) => {
-	res.json({
-		data: "Welcome to home page!"
-	})
+app.get('/upload', upload.single('image'), (req, res) => {
+	console.log(req)
 })
 
-app.get('/free', (req, res) => {
-	res.json({
-		data: "Free Route"
-	})
+server.listen(process.env.PORt, () => {
+	console.log('Server is running')
 })
-
-app.get('/paid', passport.authenticate('jwt', {session: false}), (req, res) => {
-	res.json({
-		data: "Paid route avaliable only for premium user"
-	})
-})
-
-app.get('/login', (req, res) => {
-	const {email, password} = req.body;
-	if (email === 'hello@test.com' && password === '123') {
-		res.json({
-			token
-		})
-	}
-	res.send("Invalid Credentials")
-})
-
-server.listen(process.env.PORT, () => {
-	console.log(`Server is running at http://localhost:${process.env.PORT}`)
-})
-
-
-
