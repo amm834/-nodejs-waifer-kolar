82c08f343222193dfac836273eaf1e1c76054aa7
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-25 22:42:15 +0630

    Single data inset and multiple data inset

--- 04-chat-app/index.js
+++ 04-chat-app/index.js
@@ -6,19 +6,37 @@ const errorChecker = (err, instance) => {
 	if (err) {
 		console.log(err)
 	} else {
-		console.log(err)
+		console.log("Good to go")
+		console.log(instance)
 	}
 }
 
 const createCollection = (name) => {
 	client.connect(url, (error, instance) => {
-		if (error) throw error
-		else {
+		if (error) {
+			throw error
+		} else {
 			const db = instance.db('first')
 			db.createCollection(name, errorChecker)
 		}
 	})
 }
 
+const insert = (obj) => {
+	client.connect(url, (error, instance) => {
+		if (error) {
+			throw error
+		} else {
+			const db = instance.db('first')
+			db.collection('users').insertMany(obj, errorChecker)
 
-createCollection('users')
+		}
+	})
+}
+
+insert([
+	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
+	{name: "Mg MG", email: "mgmg@gmail.com", age: 123, adresss: "Letpadan"},
+	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
+	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
+])
