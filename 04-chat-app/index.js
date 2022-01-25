1e7833d598b5c77ae43bb634f2bfd651eef6b7f3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-25 22:19:55 +0630

    Create collection

--- 04-chat-app/index.js
+++ 04-chat-app/index.js
@@ -1,10 +1,24 @@
 const client = require("mongodb").MongoClient
 const url = 'mongodb://localhost:27017/first'
 
-const connector = (err, instance) => {
-	if (err) throw err
-	console.log(instance)
+
+const errorChecker = (err, instance) => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log(err)
+	}
 }
 
-client.connect(url, connector)
+const createCollection = (name) => {
+	client.connect(url, (error, instance) => {
+		if (error) throw error
+		else {
+			const db = instance.db('first')
+			db.createCollection(name, errorChecker)
+		}
+	})
+}
 
+
+createCollection('users')
