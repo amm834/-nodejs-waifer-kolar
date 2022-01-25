ac3f72c02a00e63cdc7043e07908c1b9b17fffdf
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-25 23:22:02 +0630

    Sort data

--- 04-chat-app/index.js
+++ 04-chat-app/index.js
@@ -34,9 +34,48 @@ const insert = (obj) => {
 	})
 }
 
-insert([
-	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
-	{name: "Mg MG", email: "mgmg@gmail.com", age: 123, adresss: "Letpadan"},
-	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
-	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
-])
+// insert([
+// 	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
+// 	{name: "Mg MG", email: "mgmg@gmail.com", age: 123, adresss: "Letpadan"},
+// 	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
+// 	{name: "Mg MG", email: "mgmg@gmail.com", age: 123},
+// ])
+
+const findUser = () => {
+	client.connect(url, (error, instance) => {
+		if (error) {
+			throw error
+		} else {
+
+			const db = instance.db('first')
+			// get one
+			// db.collection('users').findOne({}, errorChecker)
+
+			// get all
+			// db.collection('users').find({}).toArray(errorChecker)
+
+			// filter by query
+			// const query = {name: "Aung Aung"}
+			// db.collection('users').find(query).toArray(errorChecker)
+
+			// get specific data of user
+			db.collection('users').find({}, {
+				projection: {
+					_id: 0,
+					name: 1
+				}
+			}).toArray(errorChecker)
+		}
+	})
+}
+
+const sortUser = () => {
+	client.connect(url, (err, instance) => {
+		const db = instance.db('first')
+		db.collection('users').find().sort({
+			name: -1
+		}).toArray(errorChecker)
+	})
+}
+
+sortUser()
