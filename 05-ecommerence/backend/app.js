f68b6dcb1fc0f248c0f8aaeba08a553cda208112
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-27 09:02:27 +0630

    Category data seeder done

--- 05-ecommerence/backend/app.js
+++ 05-ecommerence/backend/app.js
@@ -1,25 +1,6 @@
 import 'dotenv/config'
 import mongoose from "mongoose";
-import * as category from './controllers/CategoryController.js'
 
 await mongoose.connect(process.env.MONGO_URI)
 
-const catObj = {
-	id: mongoose.Types.ObjectId("61f06d1aaf74e1af2325a5c5"),
-}
 
-category.destory(catObj.id)
-	.then(res => {
-		console.log(res)
-	})
-	.catch(err => {
-		console.log(err)
-	})
-
-
-// const app = express()
-// app.use(bodyParser.json())
-//
-// app.listen(process.env.PORT, () => {
-// 	console.log(`Server is running at http://${process.env.HOST}:${process.env.PORT}`)
-// })
--- 05-ecommerence/backend/models/Category.js
+++ 05-ecommerence/backend/models/Category.js
@@ -5,6 +5,7 @@ const {Schema} = mongoose;
 const CategorySchema = new Schema({
 	id: {type: Number, required: true},
 	name: {type: String, required: true},
+	image: {type: String, required: true},
 	since: {type: Date, required: true}
 })
 
