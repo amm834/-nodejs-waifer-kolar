f8ca0e94a310e58d4d0b244b2d0b85fc6a69c0f6
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-27 10:04:03 +0630

    Product seeder completed

--- 05-ecommerence/backend/app.js
+++ 05-ecommerence/backend/app.js
@@ -1,22 +1,5 @@
 import 'dotenv/config'
 import mongoose from "mongoose";
-import {destory} from "./controllers/ProductController.js";
 
 await mongoose.connect(process.env.MONGO_URI)
 
-
-const productObj = {
-	cat_id: 1,
-	name: "Product1",
-	price: 2000,
-	description: "Hello product one",
-	since: Date.now()
-}
-
-destory(mongoose.Types.ObjectId("61f20fe5cb5380706a305476"))
-	.then(res => {
-		console.log(res)
-	})
-	.catch(err => {
-		console.log(err)
-	})
---
+++ 05-ecommerence/backend/database/seeder/product_seeder.js
@@ -0,0 +1,30 @@
+import * as fs from "fs";
+import * as path from "path";
+import {save} from "../../controllers/ProductController.js";
+import __dirname from './__dirname.cjs'
+
+const filepath = path.join(__dirname, 'data/products.json')
+
+
+fs.readFile(filepath, (err, buffer) => {
+	const products = JSON.parse(buffer);
+	products.forEach(product => {
+		const productObj = {
+			cat_id: product.id,
+			name: product.name,
+			price: product.price,
+			description: product.description,
+			since: Date.now()
+		}
+
+		console.log(productObj)
+		save(productObj).then(res => {
+			console.log(res)
+		})
+			.catch(err => {
+				console.log(err)
+			})
+
+	})
+
+})
