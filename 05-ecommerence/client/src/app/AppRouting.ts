18895f6c72197f45e63bfc290dfeebfde8e34beb
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-30 00:20:22 +0630

    Router Setup

---
+++ 05-ecommerence/client/src/app/AppRouting.ts
@@ -0,0 +1,4 @@
+import {RouterModule, Routes} from '@angular/router'
+
+const Routes: Routes = [];
+export const AppRouting = RouterModule.forRoot(Routes);
--- 05-ecommerence/client/src/app/app.module.ts
+++ 05-ecommerence/client/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavComponent} from './nav/nav.component';
+import {FooterComponent} from './footer/footer.component';
+import {HomeComponent} from './home/home.component';
+import {AppRouting} from './AppRouting';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
