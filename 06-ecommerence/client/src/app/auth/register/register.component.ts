c0907c2b67f7729d533cb25d54adb4fb7f9f2e35
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-03 23:43:07 +0630

    Register form implemted

--- 06-ecommerence/client/src/app/auth/register/register.component.ts
+++ 06-ecommerence/client/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,11 +7,40 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form = new FormGroup({
+      name: new FormControl('', [
+        Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    }
+  )
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  startRegister(data: FormData) {
+    console.log(data)
+  }
+
+  get name() {
+    return this.form.get('name')
+  }
+
+  get email() {
+    return this.form.get('email')
+  }
+
+  get password() {
+    return this.form.get('password')
   }
 
 }
